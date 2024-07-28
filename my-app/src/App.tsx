cd54a59c76330d3154fc86f781c846a61b34cebb
danbaliaba <danbaliab@gmail.com>
2024-07-28 20:50:04 +0200

    Admin Layout + All Orders Page for Admin

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import OrderPage from './components/OrderPage/OrderPage';
-import AddProductForm from './components/AddProductForm/AddProductForm';
-import CheckoutPage from './components/CheckoutPage/CheckoutPage';
-import EditProductPage from './components/EditProductPage/EditProductPage';
-import Layout from './components/Navigation/Layout';
-import HomePage from './components/HomePage/HomePage';
-import ModuleLoginRegister from './components/AuthRootComponent/ModuleLoginRegister';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import OrderPage from "./components/OrderPage/OrderPage"
+import AddProductForm from "./components/AddProductForm/AddProductForm"
+import CheckoutPage from "./components/CheckoutPage/CheckoutPage"
+import EditProductPage from "./components/EditProductPage/EditProductPage"
+import Layout from "./components/Navigation/Layout"
+import HomePage from "./components/HomePage/HomePage"
+import ModuleLoginRegister from "./components/AuthRootComponent/ModuleLoginRegister"
+import AdminLayout from "./components/AdminInterface/Layout/AdminLayout"
+import AllOrdersPage from "./components/AdminInterface/AdminAllOrders/AllOrdersPage"
 
 const App: React.FC = () => {
   return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-        <Routes>
-        <Route path="/" element={<Layout />}/>
-          <Route index element={<HomePage />} />
-          <Route path="/auth/login" element={<ModuleLoginRegister />} />
-          <Route path="/auth/register" element={<ModuleLoginRegister />} />
-          <Route path="/orders" element={<OrderPage />} />
-          <Route path="/products/new" element={<AddProductForm />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/products/edit/:id" element={<EditProductPage />} />
-        </Routes>
-      </div>
-  );
+    <div className="App">
+      <header className="App-header"></header>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<HomePage />} />
+        <Route path="/auth/login" element={<ModuleLoginRegister />} />
+        <Route path="/auth/register" element={<ModuleLoginRegister />} />
+        <Route path="/orders" element={<OrderPage />} />
+        <Route path="/products/new" element={<AddProductForm />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/products/edit/:id" element={<EditProductPage />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="/admin/orders" element={<AllOrdersPage />} />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
