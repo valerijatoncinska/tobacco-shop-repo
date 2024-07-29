4c07ab417ba6ebd46e8d126c894f2fa3aeacf009
danbaliaba <danbaliab@gmail.com>
2024-07-29 11:01:38 +0200

    Sign out button

--- my-app/src/components/AdminInterface/Navigation/Navigation.tsx
+++ my-app/src/components/AdminInterface/Navigation/Navigation.tsx
@@ -4,28 +4,37 @@ import "./Navigation.css"
 const Navigation = () => {
   return (
     <nav className="vertical-navbar">
-
-        <h4 className="text-center mt-4">Hello, Admin :)</h4>
-        <ul className="nav flex-column mt-5">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/account">Account Admin</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/catalog-products">Catalog Products</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/order-details">Order Details</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/archivated-products">
-              Archivated Products
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/admin/view-orders">View Orders</NavLink>
-          </li>
-        </ul>
-        <button className="button ms-3" >Sign out</button>
+      <h4 className="text-center mt-4">Hello, Admin :)</h4>
+      <ul className="nav flex-column mt-5">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/admin/account">
+            Account Admin
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/admin/catalog-products">
+            Catalog Products
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/admin/order-details">
+            Order Details
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/admin/archivated-products">
+            Archivated Products
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/admin/view-orders">
+            View Orders
+          </NavLink>
+        </li>
+      </ul>
+      <NavLink to="/auth/login">
+        <button className="button ms-3">Sign out</button>
+      </NavLink>
     </nav>
   )
 }
