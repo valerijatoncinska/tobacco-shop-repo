00762c78ef535973e1337f58ed7e2cf8f97cce7b
Gorishok <dasa999916@gmail.com>
2024-07-31 13:21:40 +0200

    init

--- my-app/src/components/AuthRootComponent/ModuleLoginRegister.tsx
+++ my-app/src/components/AuthRootComponent/ModuleLoginRegister.tsx
@@ -1,75 +1,72 @@
-import { useAppDispatch } from "../../app/hook"
-import axios from "axios"
-import { useState } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
-import { login } from "../../store/redux/userSlice"
-import LoginPage from "./LoginPage"
-import RegisterPage from "./RegisterPage"
+import { useLocation, useNavigate } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import { useState } from "react";
+import axios from "axios";
+import RegisterPage from "./RegisterPage";
+import { useAppDispatch } from "app/hook";
+import { login } from "store/redux/userSlice";
 
-const ModuleLoginRegister = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [repeatPassword, setRepeatPassword] = useState("")
-  const location = useLocation();
+const ModuleLR: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const locationLR = useLocation();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
     if (location.pathname === "/auth/login") {
       try {
         const userData = {
-          email,
-          password,
-        }
-        const user = await axios.post(
-          "http://localhost:8080/author/login",
-          userData,
-        )
-        await dispatch(login(user.data))
-        navigate("/")
-        console.log(user.data)
-      } catch (error) {
-        return error
-      }
-    } else {
-      if (password === repeatPassword) {
-        try {
-          const userData = {
             email,
             password,
-          }
-          const newUser = await axios.post(
-            "http:localhost:8080/author/reg",
-            userData,
-          )
-          await dispatch(login(newUser.data))
-          navigate("/")
-          console.log(newUser.data)
+          };
+          const user = await axios.post("http://localhost:8080/author/login", userData);
+          await dispatch(login(user.data))
+          navigate('/')
+          console.log(user.data)
+
+      } catch (error) {
+        return error // описание ошибки
+      }
+    } else {
+      if(password === repeatPassword) {
+        try {
+            const userData = {
+                email,
+                password
+              }
+              const newUser = await axios.post("http:localhost:8080/author/reg", userData)
+              await dispatch(login(newUser.data))
+              navigate('/')
+              console.log(newUser.data)
         } catch (error) {
-          return error
+            return error
+            
         }
       } else {
-        throw new Error("Your passwords do not match")
+        throw new Error('Your passwords do not match')
       }
     }
-  }
+  };
+
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        {location.pathname === "/auth/login" ? (
+      <div onSubmit={handleSubmit}>
+        {locationLR.pathname === "/auth/login" ? (
           <LoginPage setEmail={setEmail} setPassword={setPassword} />
-        ) : location.pathname === "/auth/register" ? (
+        ) : locationLR.pathname === "/auth/register" ? (
           <RegisterPage
             setEmail={setEmail}
             setPassword={setPassword}
             setRepeatPassword={setRepeatPassword}
           />
         ) : null}
-      </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModuleLoginRegister
+export default ModuleLR;
