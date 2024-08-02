419600e5310eae663f48f2a2e286ef8f35b0910a
Val <156693800+valerijatoncinska@users.noreply.github.com>
2024-08-02 10:39:00 +0300

    add proxy
    add proxy to vite.config.ts

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "tobacco-shop-repo",
   server: {
     open: true,
+    proxy: {
+      "/api": {
+        target: "https://smoke-shop-68y5q.ondigitalocean.app",
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
   },
 })
+
+// fetch('/api/any-chosen-api')
+// .then(response => response.json())
+// .then(data => console.log(data));
